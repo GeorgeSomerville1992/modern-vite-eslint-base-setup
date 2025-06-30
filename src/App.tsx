a2e8c3983b46807f6f0c50d76080b71aa0fd7f73
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2025-06-30 06:54:21 +0100

    add basic header/footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import './App.css';
 import { Routes, Route } from 'react-router';
 import { Home } from './pages/Home';
 import NotFound from './pages/NotFound';
+import { Header } from './components/Layout/Header';
+import { Footer } from './components/Layout/Footer';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
